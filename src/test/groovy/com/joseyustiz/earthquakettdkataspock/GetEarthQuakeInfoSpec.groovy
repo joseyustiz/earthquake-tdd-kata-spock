0eed0a2468b5f7975d96b9604558927f3b251cf5
Jose Yustiz <jose.yustiz@globallogic.com>
2019-11-28 19:07:53 -0300

    [Refactoring] get earthquake info if there was at least one earthquake between two dates

--- src/test/groovy/com/joseyustiz/earthquakettdkataspock/GetEarthQuakeInfoSpec.groovy
+++ src/test/groovy/com/joseyustiz/earthquakettdkataspock/GetEarthQuakeInfoSpec.groovy
@@ -2,29 +2,64 @@ package com.joseyustiz.earthquakettdkataspock
 
 import spock.lang.Specification
 
-class GetEarthQuakeInfoSpec extends Specification {
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.stream.Collectors
 
-    def "there is no earthquake info between two dates"() {
+class GetEarthquakeInfoSpec extends Specification {
+
+    def "there is no info when there was no earthquake between two dates"() {
         given:
-        GetEarthQuakeInfoUseCase earthQuakeInfoService = new GetEarthQuakeInfoService();
+        GetEarthquakeInfoUseCase earthquakeInfoService = new GetEarthquakeInfoService();
         def startDate = "2019-10-13"
         def endDate = "2019-10-13"
 
         when:
-        def earthQuakeInfo = earthQuakeInfoService.getInfoBetweenDates(startDate, endDate)
+        def earthquakeInfo = earthquakeInfoService.getInfoBetweenDates(startDate, endDate)
 
         then:
-        earthQuakeInfo.isEmpty()
+        earthquakeInfo.isEmpty()
     }
 
-    interface GetEarthQuakeInfoUseCase {
+    def "get earthquake info if there was at least one earthquake between two dates"(){
+        given:
+        GetEarthquakeInfoUseCase earthquakeInfoService = new GetEarthquakeInfoService();
+        def startDate = "2019-10-13"
+        def endDate = "2019-10-14"
+
+        when:
+        def earthquakes = earthquakeInfoService.getInfoBetweenDates(startDate, endDate)
+
+        then:
+        !earthquakes.isEmpty()
+
+    }
+
+    interface GetEarthquakeInfoUseCase {
         List<String> getInfoBetweenDates(String startDate, String endDate);
     }
 
-    class GetEarthQuakeInfoService implements GetEarthQuakeInfoUseCase {
+    class GetEarthquakeInfoService implements GetEarthquakeInfoUseCase {
+        private Map<LocalDate, List<String>> earthquakes;
+        GetEarthquakeInfoService() {
+            this.earthquakes = new HashMap();
+            earthquakes.put(LocalDate.parse("2019-10-14"),Collections.singletonList("Earthquake 1"));
+        }
 
         List<String> getInfoBetweenDates(String startDate, String endDate) {
-            return new ArrayList();
+            LocalDate sDate= LocalDate.parse(startDate);
+            LocalDate eDate= LocalDate.parse(endDate).plusDays(1);
+
+            Set<LocalDate> datesRange = sDate.datesUntil(eDate).collect(Collectors.toSet());
+            System.out.println(datesRange);
+
+            List<String> earthquakeInfoInDateRange = new ArrayList();
+
+            for (LocalDate date : datesRange){
+                if(earthquakes.containsKey(date))
+                earthquakeInfoInDateRange.addAll(earthquakes.get(date));
+            }
+            return earthquakeInfoInDateRange;
         }
     }
 }
