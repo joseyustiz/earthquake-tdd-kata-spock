262daeec8c8c069521db33c92fa22beb520a99f6
Jose Yustiz <jose.yustiz@globallogic.com>
2019-11-28 15:20:36 -0300

    [RED] there is no earthquake info between two dates

--- src/test/groovy/com/joseyustiz/earthquakettdkataspock/GetEarthQuakeInfoSpec.groovy
+++ src/test/groovy/com/joseyustiz/earthquakettdkataspock/GetEarthQuakeInfoSpec.groovy
@@ -2,5 +2,29 @@ package com.joseyustiz.earthquakettdkataspock
 
 import spock.lang.Specification
 
-class GetEarthQuakeInfoSpec extends Specification{
+class GetEarthQuakeInfoSpec extends Specification {
+
+    def "there is no earthquake info between two dates"() {
+        given:
+        LoadEarthQuakeInfoPort earthQuakeInfoReader = new EarthQuakeInfoAdapter();
+        def startDate = "2019-10-13"
+        def endDate = "2019-10-13"
+        when:
+        def earthQuakeInfo = earthQuakeInfoReader.getEarthQuakeInfoBetweenDate(startDate, endDate)
+        then:
+        earthQuakeInfo.isEmpty()
+    }
+
+    interface LoadEarthQuakeInfoPort {
+        List<String> getEarthQuakeInfoBetweenDate(String startDate, String endDate);
+    }
+
+    class EarthQuakeInfoAdapter implements LoadEarthQuakeInfoPort {
+
+
+        @Override
+        List<String> getEarthQuakeInfoBetweenDate(String startDate, String endDate) {
+            return null
+        }
+    }
 }
