9ca8d87e762439002a5670d79dabebc0b85bf4b5
Jose Yustiz <jose.yustiz@gmail.com>
2019-11-29 15:47:25 -0300

    [REFACTOR] Substituted InMemoryDatabase for a Mock in the domain project

--- domain/src/test/groovy/com/joseyustiz/earthquakeinfo/GetEarthQuakeInfoSpec.groovy
+++ domain/src/test/groovy/com/joseyustiz/earthquakeinfo/GetEarthQuakeInfoSpec.groovy
@@ -1,6 +1,7 @@
 package com.joseyustiz.earthquakeinfo
 
 import com.joseyustiz.earthquakeinfo.application.port.in.GetEarthquakeInfoUseCase
+import com.joseyustiz.earthquakeinfo.application.port.out.LoadEarthquakeInfoPort
 import com.joseyustiz.earthquakeinfo.application.service.GetEarthquakeInfoService
 import spock.lang.Shared
 import spock.lang.Specification
@@ -17,15 +18,22 @@ class GetEarthquakeInfoSpec extends Specification {
     private def earthquake1
     @Shared
     private def earthquake2
+    @Shared
+    private def mockedEarthquakeDatabase = Mock(LoadEarthquakeInfoPort)
 
     def setupSpec() {
-        def inMemoryDB = new EarthquakeInfoInMemoryDatabaseAdapter()
         earthquake1 = new EarthquakeInfo("Earthquake 1", parse("2019-10-14"), 6.0)
-        inMemoryDB.addEarthquakeInfo(earthquake1)
         earthquake2 = new EarthquakeInfo("Earthquake 2", parse("2019-10-15"), 7.0)
-        inMemoryDB.addEarthquakeInfo(earthquake2)
 
-        earthquakeInfoService = new GetEarthquakeInfoService(inMemoryDB)
+        mockedEarthquakeDatabase.getInfoBetweenDates("2019-10-13", "2019-10-13") >> []
+        mockedEarthquakeDatabase.getInfoBetweenDates("2019-10-13", "2019-10-14") >> [earthquake1]
+        mockedEarthquakeDatabase.getInfoBetweenDates("2019-10-13", "2019-10-15") >> [earthquake1, earthquake2]
+
+        mockedEarthquakeDatabase.getInfoBetweenMagnitudes(1.5, 2.0) >> []
+        mockedEarthquakeDatabase.getInfoBetweenMagnitudes(6.5, 7.0) >> [earthquake2]
+        mockedEarthquakeDatabase.getInfoBetweenMagnitudes(6.0, 7.0) >> [earthquake1, earthquake2]
+
+        earthquakeInfoService = new GetEarthquakeInfoService(mockedEarthquakeDatabase)
     }
 
     @Unroll("#message")
@@ -39,7 +47,7 @@ class GetEarthquakeInfoSpec extends Specification {
         "2019-10-13" | "2019-10-14" | [earthquake1]
         "2019-10-13" | "2019-10-15" | [earthquake1, earthquake2]
 
-        message = "Retrieved " + earthquakesInfo.size() + " earthquake(s) info given that happened " + earthquakesInfo.size() +" from " + startDate + " to " + endDate
+        message = "Retrieved " + earthquakesInfo.size() + " earthquake(s) info given that happened " + earthquakesInfo.size() + " from " + startDate + " to " + endDate
     }
 
     @Unroll("#message")
@@ -52,6 +60,6 @@ class GetEarthquakeInfoSpec extends Specification {
         1.5          | 2.0          | []
         6.5          | 7.0          | [earthquake2]
         6.0          | 7.0          | [earthquake1, earthquake2]
-        message = "Retrieved " + earthquakesInfo.size() + " earthquake(s) info given that happened " + earthquakesInfo.size() +" with magnitude between " + minMagnitude + " to " + maxMagnitude
+        message = "Retrieved " + earthquakesInfo.size() + " earthquake(s) info given that happened " + earthquakesInfo.size() + " with magnitude between " + minMagnitude + " to " + maxMagnitude
     }
 }
