86b27969da4ade184213f560b364b79ba0266d85
Jose Yustiz <jose.yustiz@gmail.com>
2019-12-01 18:46:12 -0300

    [RED] get earthquake info from database service between two dates by calling REST API

--- infrastructure/src/main/java/com/joseyustiz/earthquakeinfo/adapter/web/GetEarthquakeInfoController.java
+++ infrastructure/src/main/java/com/joseyustiz/earthquakeinfo/adapter/web/GetEarthquakeInfoController.java
@@ -1,7 +1,19 @@
 package com.joseyustiz.earthquakeinfo.adapter.web;
 
+import com.joseyustiz.earthquakeinfo.application.port.in.GetEarthquakeInfoUseCase;
+import com.joseyustiz.earthquakeinfo.model.EarthquakeInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class GetEarthquakeInfoController {
+    private final GetEarthquakeInfoUseCase service;
+
+    public List<EarthquakeInfo> getInfoBetweenDates(LocalDate localDate, LocalDate localDate1) {
+        return null;
+    }
 }
