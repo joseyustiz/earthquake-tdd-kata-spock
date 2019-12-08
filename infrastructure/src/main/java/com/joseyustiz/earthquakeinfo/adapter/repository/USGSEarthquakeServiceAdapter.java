45c64fd3c7fb040def466a1b9ddc646d43f84255
Jose Yustiz <jose.yustiz@gmail.com>
2019-12-07 21:58:15 -0300

    [REFACTOR] get earthquake info between two magnitudes from the USGS Earthquake Service

--- infrastructure/src/main/java/com/joseyustiz/earthquakeinfo/adapter/repository/USGSEarthquakeServiceAdapter.java
+++ infrastructure/src/main/java/com/joseyustiz/earthquakeinfo/adapter/repository/USGSEarthquakeServiceAdapter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joseyustiz.earthquakeinfo.application.port.out.LoadEarthquakeInfoPort;
 import com.joseyustiz.earthquakeinfo.model.EarthquakeInfo;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,68 +17,54 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
+@RequiredArgsConstructor
 public class USGSEarthquakeServiceAdapter implements LoadEarthquakeInfoPort {
-    private RestTemplate restTemplate;
-    private URL serviceUrl;
-
-    public USGSEarthquakeServiceAdapter(RestTemplate restTemplate, URL serviceUrl) {
-        this.restTemplate = restTemplate;
-        this.serviceUrl = serviceUrl;
-    }
+    private final RestTemplate restTemplate;
+    private final URL serviceUrl;
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public List<EarthquakeInfo> getInfoBetweenDates(LocalDate startDate, LocalDate endDate) {
         String url = serviceUrl.toString() + "&starttime=" + startDate + "&endtime=" + endDate;
+        return getEarthquakesInfo(url);
+    }
+
+    private List<EarthquakeInfo> getEarthquakesInfo(String url) {
         UUGSEarthquakeInfoQueryResult serviceResponse = restTemplate.getForObject(url, UUGSEarthquakeInfoQueryResult.class);
-        ObjectMapper objectMapper = new ObjectMapper();
         assert serviceResponse != null;
         return serviceResponse.getFeatures().stream()
-                .map(e -> {
-                    try {
-                        return EarthquakeInfo.builder()
-                                .magnitude(e.getProperties().getMag())
-                                .country(getCountry(e.getProperties().getPlace()))
-                                .date(getLocalDate(e.getProperties().getTime()))
-                                .info(objectMapper.writeValueAsString(e))
-                                .build();
-                    } catch (JsonProcessingException ex) {
-                        log.error("Problem converting USGS Earthquake Feature to json", ex);
-                    }
-                    return new EarthquakeInfo();
-                })
+                .map(e -> getEarthquakeInfoFor(e))
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private LocalDate getLocalDate(long time) {
+    private EarthquakeInfo getEarthquakeInfoFor(Feature e) {
+        try {
+            return EarthquakeInfo.builder()
+                    .magnitude(e.getProperties().getMag())
+                    .country(exytractCountry(e.getProperties().getPlace()))
+                    .date(getLocalDateForEpochMilli(e.getProperties().getTime()))
+                    .info(OBJECT_MAPPER.writeValueAsString(e))
+                    .build();
+        } catch (JsonProcessingException ex) {
+            log.error("Problem converting USGS Earthquake Feature to json", ex);
+        }
+        return new EarthquakeInfo();
+    }
+
+    private LocalDate getLocalDateForEpochMilli(long time) {
         return Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    private String getCountry(String place) {
+    private String exytractCountry(String place) {
         String[] token = place.split(",");
-        return token.length > 0 ? token[token.length-1].trim(): "";
+        return token.length > 0 ? token[token.length - 1].trim() : "";
     }
 
     @Override
     public List<EarthquakeInfo> getInfoBetweenMagnitudes(BigDecimal minMagnitude, BigDecimal maxMagnitude) {
         String url = serviceUrl.toString() + "&minmagnitude=" + minMagnitude + "&maxmagnitude=" + maxMagnitude;
-        UUGSEarthquakeInfoQueryResult serviceResponse = restTemplate.getForObject(url, UUGSEarthquakeInfoQueryResult.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        assert serviceResponse != null;
-        return serviceResponse.getFeatures().stream()
-                .map(e -> {
-                    try {
-                        return EarthquakeInfo.builder()
-                                .magnitude(e.getProperties().getMag())
-                                .country(getCountry(e.getProperties().getPlace()))
-                                .date(getLocalDate(e.getProperties().getTime()))
-                                .info(objectMapper.writeValueAsString(e))
-                                .build();
-                    } catch (JsonProcessingException ex) {
-                        log.error("Problem converting USGS Earthquake Feature to json", ex);
-                    }
-                    return new EarthquakeInfo();
-                })
-                .collect(Collectors.toUnmodifiableList());    }
+        return getEarthquakesInfo(url);
+    }
 
     @Override
     public List<EarthquakeInfo> getAllEarthquakesInfo() {
